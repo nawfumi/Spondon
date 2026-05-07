3f9083000abc5e1d2fc625afcb3415c50bc96903
ashgorhythm <ashgorhythm@github.com>
2026-05-07 23:46:57 +0600

    Add empty secret check for google-services.json and bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.spondon.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
