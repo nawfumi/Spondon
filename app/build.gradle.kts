6dd7723076ecdadd05d8fbf6bacdba8a33a03d2d
ashgorhythm <ashgorhythm@github.com>
2026-05-30 20:30:32 +0600

    Bump version to 1.0.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.spondon.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 19
-        versionName = "1.0.18"
+        versionCode = 20
+        versionName = "1.0.19"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GITHUB_TOKEN", "\"${githubToken}\"")
     }
